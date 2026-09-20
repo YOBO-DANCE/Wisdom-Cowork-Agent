68b413eebe2de926865500350dc9daea80d37bc1
Tanmay Hathile <bloxybugassi@gmail.com>
2026-09-20 12:32:30 +0530

    add pathlib examples and create tempfile.txt for demonstration

---
+++ Learning/pathlib/basics.py
@@ -0,0 +1,36 @@
+import os
+from pathlib import Path
+
+# print(os.getcwd())
+# print(Path.cwd())
+
+# for f in os.listdir():
+#     print(f)
+
+# for p in Path().iterdir():
+#     print(p)
+
+# my_file_os = os.path.abspath("main.py")
+# my_file_pathlib = Path("main.py")
+# my_dir = Path("Learning")
+
+# print(my_file_os)
+# print(my_file_pathlib)
+
+
+# new_file = my_dir / __file__ / "Hello World.txt"
+
+# print(new_file.parent.resolve())
+# print(new_file.parent.absolute())
+
+# user_dic = Path("~").expanduser()
+# print(user_dic)
+
+# for files in user_dic.rglob("*vscode*", case_sensitive=False):
+#     print(files)
+
+p = Path("tempfile.txt")
+p.touch()
+
+with p.open("w") as p:
+    p.write("Hello World")
