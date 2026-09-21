7d805bd8052c50ae6b4047bf930369bcbf982a9d
Tanmay Hathile <bloxybugassi@gmail.com>
2026-09-21 19:51:08 +0530

    refactor subprocess tutorial: comment out example code and add new Exercise1.py file

--- Learning/subprocess/subprocess_tut.py
+++ Learning/subprocess/subprocess_tut.py
@@ -1,5 +1,7 @@
 import subprocess
 
-p1 = subprocess.run('dir', shell=True, capture_output=True, text=True)
+# p1 = subprocess.run('dir', shell=True, capture_output=True, text=True)
 
-print(p1.stdout)
+# print(p1.stdout)
+
+# Popen
